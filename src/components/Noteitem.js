a0f216e356d4d1429dd94db1f666e17a13609627
Vardhan <vardhandeepak1234@gmail.com>
2022-05-08 09:13:52 +0530

    Added Edit Note

--- src/components/Noteitem.js
+++ src/components/Noteitem.js
@@ -5,7 +5,7 @@ import noteContext from '../context/notes/noteContext';
 const Noteitem = (props) => {
     const context = useContext(noteContext);
     const { deleteNote } = context;
-    const { note } = props;
+    const { note, updateNote } = props;
     return (
 
 
@@ -15,7 +15,7 @@ const Noteitem = (props) => {
                     <h5 className="card-title">{note.title}</h5>
                     <p className="card-text">{note.description}</p>
                     <i className="fa-solid fa-trash-can mx-3" onClick={() => { deleteNote(note._id) }}></i>
-                    <i className="fa-solid fa-pen-to-square mx-3"></i>
+                    <i className="fa-solid fa-pen-to-square mx-3" onClick={() => { updateNote(note) }}></i>
                 </div>
             </div>
         </div>
