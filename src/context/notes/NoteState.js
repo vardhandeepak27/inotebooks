99ce41e11d6015db16ffbd1ace1125a26f065c19
Vardhan <vardhandeepak1234@gmail.com>
2022-05-08 09:34:39 +0530

    Made a few changes in edit note

--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -71,17 +71,20 @@ const NoteState = (props) => {
         });
         const json = await response.json();
 
-
+        let newNotes = JSON.parse(JSON.stringify(notes))
         //Logic to edit in client
-        for (let index = 0; index < notes.length; index++) {
-            const element = notes[index];
+        for (let index = 0; index < newNotes.length; index++) {
+            const element = newNotes[index];
             if (element._id === id) {
-                element.title = title;
-                element.description = description;
-                element.tag = tag;
+                newNotes[index].title = title;
+                newNotes[index].description = description;
+                newNotes[index].tag = tag;
+                break;
             }
 
+
         }
+        setNotes(newNotes);
 
     }
 
